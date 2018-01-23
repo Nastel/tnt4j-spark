dfdc057a12ac35b66b6689b54bfcc3dc10b07ebd
akausinis <andrius.kausinis@singleton-labs.lt>
2018-01-23 17:07:47 +0200

    Copyright updated.

--- src/main/java/com/jkoolcloud/tnt4j/spark/TNTSparkListener.java
+++ src/main/java/com/jkoolcloud/tnt4j/spark/TNTSparkListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 JKOOL, LLC.
+ * Copyright 2014-2018 JKOOL, LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,8 @@ import com.jkoolcloud.tnt4j.tracker.TrackingEvent;
  */
 public class TNTSparkListener implements SparkListenerInterface {
 
-	private ConcurrentHashMap<String, TrackingActivity> activityMap = new ConcurrentHashMap<String, TrackingActivity>(89);
+	private ConcurrentHashMap<String, TrackingActivity> activityMap = new ConcurrentHashMap<String, TrackingActivity>(
+			89);
 	TrackingLogger logger;
 	TrackingActivity applActivity;
 
